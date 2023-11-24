b37a561085a87f921a06ed0af39961a042108da5
mattromawak <145722379+mattromawak@users.noreply.github.com>
2023-11-24 09:47:22 +0800

    Update option.py

--- handgesturefinalsad/option.py
+++ handgesturefinalsad/option.py
@@ -14,7 +14,7 @@ root.title("Choose Hand Detection")
 root.geometry("300x200") 
 root.configure(bg='#ADD8E6') 
 
-label = tk.Label(root, text="Select a script to run:", font=("Arial", 14), bg='#ADD8E6')
+label = tk.Label(root, text="Select mode:", font=("Arial", 14), bg='#ADD8E6')
 label.pack(pady=20)
 
 button1 = tk.Button(root, text="Fingercount", command=run_fingercount, font=("Arial", 12), bg='#ADD8E6', fg='#000000')
